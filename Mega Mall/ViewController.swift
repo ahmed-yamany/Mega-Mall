23bd8c0e92d735b8c7214b22d898b5fa24b17c30
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2023-10-21 09:50:44 +0300

    Initial Commit

---
+++ Mega Mall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mega Mall
+//
+//  Created by Ahmed Yamany on 21/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
